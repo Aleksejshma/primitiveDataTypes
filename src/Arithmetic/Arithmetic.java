875fb230230300d7139186932b381f810b940765
Alexsej <aleksejshmakov7@gmail.com>
2023-11-14 21:13:42 +0300

    Initial commit

---
+++ src/Arithmetic/Arithmetic.java
@@ -0,0 +1,51 @@
+package Arithmetic;
+
+public class Arithmetic {
+
+    public static void main(String[] args) {
+//        byte sum = 21 + 45;
+//        System.out.println(sum);  // Вычисление успешно.
+//
+//        byte sumInt = 32 + 12_432;
+//
+//        System.out.println(sumInt);  // Вычесление не успешно, размерность байта мала.
+//
+//        int sumInt = 12_345 + 32_653;
+//
+//        System.out.println(sumInt);// Вычисление успешно.
+//
+//        byte sumDouble = 12 + 2.43;
+//
+//        System.out.println(sumDouble);  // Вычисление не успешно, необходим тип данных double.
+//
+//       byte q = 12;
+//       int w = 23_563;
+//       int e = 3;
+//       short r = 43;
+//       int wq =(q + w) * e +r;
+//        System.out.println((long)wq);  // Вычисление успешно. Ответ 70_768.
+//
+//        byte w = 12 + 34;
+//        double q = 2.4375 / 1.2;
+//        int e = 12_234 * 2 - 213 + 235;
+//        int s = 125 % 10;
+//        double percent = 0.15;
+//        double ew = w + q * e * s * percent;
+//        System.out.println(ew);  // Вычисление успешно. Остааток деления нацело и проценты сработали верно.Ответ: 37354.984375
+//
+//
+//        char e = 'u';
+//        int q = 12_833;
+//        int r = e + q;
+//        System.out.println(r); //  Сложение char и int успешно. Ответ: 12950.
+//
+//        char q = 'o';
+//        char w = '[';
+//        char e = '#';
+//        int qwe = q * w / e;
+//        System.out.println(qwe); // Вычисление прошло успешно.
+
+    }
+
+
+}
