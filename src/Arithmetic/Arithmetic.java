7d93dac378ad22a38f8e1546f1f8f1aae9dee49e
Alexsej <aleksejshmakov7@gmail.com>
2023-11-14 22:02:40 +0300

    Correct number

--- src/Arithmetic/Arithmetic.java
+++ src/Arithmetic/Arithmetic.java
@@ -6,11 +6,11 @@ public class Arithmetic {
 //        byte sum = 21 + 45;
 //        System.out.println(sum);  // Вычисление успешно.
 //
-//        byte sumInt = 32 + 12_432;
+//        byte sumInt = 32 + 12432;
 //
 //        System.out.println(sumInt);  // Вычесление не успешно, размерность байта мала.
 //
-//        int sumInt = 12_345 + 32_653;
+//        int sumInt = 12345 + 32653;
 //
 //        System.out.println(sumInt);// Вычисление успешно.
 //
@@ -19,23 +19,23 @@ public class Arithmetic {
 //        System.out.println(sumDouble);  // Вычисление не успешно, необходим тип данных double.
 //
 //       byte q = 12;
-//       int w = 23_563;
+//       int w = 23563;
 //       int e = 3;
 //       short r = 43;
 //       int wq =(q + w) * e +r;
 //        System.out.println((long)wq);  // Вычисление успешно. Ответ 70_768.
 //
-//        byte w = 12 + 34;
-//        double q = 2.4375 / 1.2;
-//        int e = 12_234 * 2 - 213 + 235;
-//        int s = 125 % 10;
-//        double percent = 0.15;
-//        double ew = w + q * e * s * percent;
-//        System.out.println(ew);  // Вычисление успешно. Остааток деления нацело и проценты сработали верно.Ответ: 37354.984375
+        byte w = 12 + 34;
+        double q = 2.4375 / 1.2;
+        int e = 12234 * 2 - 213 + 235;
+        int s = 125 % 10;
+        double percent = 0.15;
+        double ew = w + q * e * s * percent;
+        System.out.println(ew);  // Вычисление успешно. Остааток деления нацело и проценты сработали верно.Ответ: 37354.984375
 //
 //
 //        char e = 'u';
-//        int q = 12_833;
+//        int q = 12833;
 //        int r = e + q;
 //        System.out.println(r); //  Сложение char и int успешно. Ответ: 12950.
 //
@@ -44,8 +44,8 @@ public class Arithmetic {
 //        char e = '#';
 //        int qwe = q * w / e;
 //        System.out.println(qwe); // Вычисление прошло успешно.
-
+//
     }
-
-
+//
+//
 }
